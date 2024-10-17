e9ff84ab5ceafc613a64bec22f9c9cf8d15a936e
Yohan Kasad <161930272+Mrbob213@users.noreply.github.com>
2024-10-17 00:53:17 -0700

    Login Created

--- Grocery Website/src/pages/RegisterLoginPage.jsx
+++ Grocery Website/src/pages/RegisterLoginPage.jsx
@@ -1,13 +1,18 @@
+import styles from "../styles/RegisterLogin.module.css"; // Import the CSS module
 
-export default function RegisterLoginPage(){
+export default function RegisterLoginPage() {
     return (
-
-        <>
-        
-        <div>This is the Register/Login page</div>
-        
-        
-        </>
-
-    )
+        <div id="main" className={styles.main}>
+            <div className={styles["login-box"]}>
+                <h1>Log in</h1>
+                <label htmlFor="username"><h2>Username: </h2></label>
+                <input type="text" name="username"></input>
+                <label htmlFor="password"><h2>Password: </h2></label>
+                <input type="password" name="Password"></input>
+                <div className={styles.buttonSpacing}>
+                <input type="submit" value="Login"></input>
+                </div>
+            </div>
+        </div>
+    );
 }
